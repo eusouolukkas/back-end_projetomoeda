c7a83a6c3b8adf6bb2a12f052e61fb17e3957af4
Fabiano César <fabiano@tecsagroup.com.br>
2023-07-27 22:32:28 -0300

    chore: adiciona cors

--- src/main/config/express.config.ts
+++ src/main/config/express.config.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { userRoutes } from "../../app/features/users/routes/user.routes";
 import { coinsRoutes } from "../../app/features/coins/routes/coins.routes";
 
 export const createServer = () => {
     const app = express();
     app.use(express.json());
+    app.use(cors());
 
     app.use("/users", userRoutes);
     app.use("/coins", coinsRoutes);
